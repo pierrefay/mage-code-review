37956c1a9ad4b658eca24f56fb635b1cc79924b1
Pierre FAY <contact@pierrefay.fr>
2015-03-23 14:58:32 +0100

    modifications

--- python/test.py
+++ python/test.py
@@ -6,15 +6,18 @@ audit = Audit()
 dossierLog =  '/home/pierre/Documents/Projects/MagentoCodeAnalysis/logs/'
 path = '/home/pierre/Documents/Projects/courir/courir/'
 
-#analyse nombre de modules
-audit.nbrNamespaceAndModule(path,dossierLog)
 
-#RECHERCHE
-resultTpl = audit.analyserTemplates(path+"app/design/frontend/", dossierLog, None)
+#on lance toutes les recherches
+result = audit.analyserLeCode(path, dossierLog, None)
+etatPlateforme = audit.nbrNamespaceAndModule(path,dossierLog)
 
-###
-###  Debut du fichier
-###
+
+#on construit le rapport
+
+
+############################
+###   Debut du fichier   ###
+############################
 
 loadsTemplatesLog = open(dossierLog+"load-in-template.html", "w")
 loadsTemplatesLog.write("<head>")
@@ -22,55 +25,139 @@ loadsTemplatesLog.write("<meta http-equiv=\"Content-Type\" content=\"text/html; 
 loadsTemplatesLog.write("</head>")
 loadsTemplatesLog.write("<body>")
 
+############################
+###     ETAT GENERAL     ###
+############################
+loadsTemplatesLog.write("<h2>Etat général de la plateforme</h2>\n")
 ### Analyse des modules ###
+loadsTemplatesLog.write("<h3>Modules et namespaces</h3>\n")
+loadsTemplatesLog.write(etatPlateforme)
 
+############################
+###      TEMPLATES       ###
+############################
+loadsTemplatesLog.write("<h2>Etat des templates</h2>\n")
+
+#############################
 ### Loads dans templates  ###
-loadsTemplatesLog.write("<h2>Loads dans les templates</h2>\n")
+loadsTemplatesLog.write("<h3>Loads dans les templates</h3>\n")
 loadsTemplatesLog.write("<p>Ces appels prennent beaucoup de charges et ne permettent pas à magento d'utiliser le cache correctement. Il faut absolument sortir ces appels des fichiers des templates (.phtml) et les effectuer correctement dans les blocs.</p>\n")
 loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
 loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
 
-tab=resultTpl['search_for_load']
+tab=result['template_search_for_load']
 for res in tab:
 	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
-
 loadsTemplatesLog.write("</table>\n")
 
+############################
 ### getBlock dans templates  ###
-loadsTemplatesLog.write("<h2>GetBlock dans les templates</h2>\n")
+loadsTemplatesLog.write("<h3>GetBlock dans les templates</h3>\n")
 loadsTemplatesLog.write("<p>Ces appels empèchent magento d'utiliser le cache correctement. Il faut absolument sortir ces appels des fichiers des templates (.phtml) et utiliser correctement les layouts.</p>\n")
 loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
 loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
 
-tab=resultTpl['search_for_getblock']
+tab=result['template_search_for_getblock']
 for res in tab:
 	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
-
 loadsTemplatesLog.write("</table>\n")
 
+###################################
 ### createBlock dans templates  ###
-loadsTemplatesLog.write("<h2>CreateBlock dans les templates</h2>\n")
+loadsTemplatesLog.write("<h3>CreateBlock dans les templates</h3>\n")
 loadsTemplatesLog.write("<p>Ces blocs ne sont pas crées correctement, leur création devrait être dans le layout ou dans un contrôleur</p>\n")
 loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
 loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
 
-tab=resultTpl['search_for_createblock']
+tab=result['template_search_for_createblock']
 for res in tab:
 	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
-
 loadsTemplatesLog.write("</table>\n")
 
+
+###################################
 ### new dans templates  ###
-loadsTemplatesLog.write("<h2>New dans les templates</h2>\n")
+loadsTemplatesLog.write("<h3>New dans les templates</h3>\n")
 loadsTemplatesLog.write("<p>On utilise jamais une instanciation directement via un \"new\" sur magento.</p>\n")
 loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
 loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
 
-tab=resultTpl['search_for_new']
+tab=result['template_search_for_new']
 for res in tab:
 	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
-
 loadsTemplatesLog.write("</table>\n")
+
+###################################
+### PHP globals dans templates  ###
+loadsTemplatesLog.write("<h3>Appels PHP globals les templates</h3>\n")
+loadsTemplatesLog.write("<p>On utilise jamais ces fonctions dans magento pour des raisons de sécurité. à la place de $_POST préférer $this->getRequest()->getPost() par exemple.</p>\n")
+loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
+loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
+
+tab=result['template_global_php']
+for res in tab:
+	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
+loadsTemplatesLog.write("</table>\n")
+
+###################################
+### Fonctions mysql dans templates  ###
+loadsTemplatesLog.write("<h3>Appels aux fonctions mysql dans les templates</h3>\n")
+loadsTemplatesLog.write("<p>Magento utilise des objets pour dialoguer directement avec la base de donnée, il ne faut jamais utiliser les fonctions \"mysql_\" de php.</p>\n")
+loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
+loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
+
+tab=result['template_mysql']
+for res in tab:
+	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
+loadsTemplatesLog.write("</table>\n")
+
+############################
+###         CODE         ###
+############################
+
+loadsTemplatesLog.write("<h2>Etat du code</h2>\n")
+
+#########################
+### new dans le code  ###
+loadsTemplatesLog.write("<h3>New dans le code</h3>\n")
+loadsTemplatesLog.write("<p>On utilise jamais une instanciation directement via un \"new\" sur magento.</p>\n")
+loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
+loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
+
+tab=result['code_search_for_new']
+for res in tab:
+	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
+loadsTemplatesLog.write("</table>\n")
+
+#################################
+### PHP globals dans le code  ###
+loadsTemplatesLog.write("<h3>Appels PHP globals le code</h3>\n")
+loadsTemplatesLog.write("<p>On utilise jamais ces fonctions dans magento pour des raisons de sécurité. à la place de $_POST préférer $this->getRequest()->getPost() par exemple.</p>\n")
+loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
+loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
+
+tab=result['code_global_php']
+for res in tab:
+	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
+loadsTemplatesLog.write("</table>\n")
+
+#################################
+###    Mysql dans le code  ###
+loadsTemplatesLog.write("<h3>Appels aux fonctions mysql dans le code</h3>\n")
+loadsTemplatesLog.write("<p>Magento utilise des objets pour dialoguer directement avec la base de donnée, il ne faut jamais utiliser les fonctions \"mysql_\" de php.</p>\n")
+loadsTemplatesLog.write("<table style=\"border:1px solid #000;text-align:left;\">\n")
+loadsTemplatesLog.write("<tr><th>Fichier</th><th style=\"width:100px;\">Ligne</th><th>Code concerné</th></tr>\n")
+
+tab=result['code_mysql']
+for res in tab:
+	loadsTemplatesLog.write("<tr><td>"+res[0]['path']+"</td><td>"+res[0]['ligne']+"</td><td>"+res[0]['contents']+"</td></tr>\n")
+loadsTemplatesLog.write("</table>\n")
+
+
+############################
+###     ANALYSE BDD      ###
+############################
+
 
 ###
 ### Fin du fichier
--- python/utils/audit.py
+++ python/utils/audit.py
@@ -11,9 +11,7 @@ class Audit:
 
 	# Fonction qui liste le nombre de namespaces et de modules	
 	def nbrNamespaceAndModule(self, path, dossierLog):
-		retour="-----------------------\n"
-		retour+="Analyses des modules  \n"
-		retour+="-----------------------\n"
+		retour=""
 		rep = Repertoire()
 		codePoolDirs = os.listdir(path+'app/code/')			
 		nombreDeModulesTotal = 0
@@ -26,61 +24,106 @@ class Audit:
 			for namespace in namespaceDirs:
 				nombreDeModules += rep.countFolders(path+'app/code/'+codePool+'/'+namespace+'/')
 			nombreDeModulesTotal = nombreDeModulesTotal + nombreDeModules
-			retour +=  "> codePool : "+codePool+" ("+str(nombreDeNamespaces)+" namespaces,"+str(nombreDeModules)+" modules) \n"
+			retour +=  "- codePool : "+codePool+" ("+str(nombreDeNamespaces)+" namespaces, "+str(nombreDeModules)+" modules) <br />\n"
 		retour+="\n"
-		retour+="Total  :  "+str(nombreDeNamespacesTotal)+" namespaces et "+str(nombreDeModulesTotal)+" modules. \n"
-		retour+="-----------------------\n"
+		retour+="<br  />  <strong>Total  :</strong>  "+str(nombreDeNamespacesTotal)+" namespaces et "+str(nombreDeModulesTotal)+" modules. \n"
 		
-		fichierlog = open(dossierLog+"etatModules.txt",'w+')
-		fichierlog.write(retour)	
-		fichierlog.close()	
 		return retour
 
 
 	# Fonction qui analyse les templates
-	def analyserTemplates(self, path, dossierLog, tab):	
+	def analyserLeCode(self, path, dossierLog, tab):	
 		#DEBUT LOADS IN TEMPLATES			
 		rep = Repertoire()	
 		if(tab is None):
 			results = {}
-			results['search_for_load']=[]
-			results['search_for_getblock']=[]
-			results['search_for_createblock']=[]
-			results['search_for_new']=[]
+			#code
+			results['code_search_for_new']=[]
+			results['code_global_php']=[]
+			results['code_mysql']=[]
+
+			#templates
+			results['template_search_for_load']=[]
+			results['template_search_for_getblock']=[]
+			results['template_search_for_createblock']=[]
+			results['template_search_for_new']=[]
+			results['template_global_php']=[]
+			results['template_mysql']=[]
 		else:
 			results=tab
 
 		dirs = os.listdir(path)	
-		for ligne in dirs:			
+		for ligne in dirs:		
 			if os.path.isdir(path+ligne):
-				self.analyserTemplates(path+ligne+"/", dossierLog, results)
+				self.analyserLeCode(path+ligne+"/", dossierLog, results)
 			else:
 				#ici on est dans chaque fichier du dossier
-				if(ligne.endswith('.phtml')):
+				if(re.search(r"app",path+ligne)):
+					#si dans le code
+					if(re.search(r"app\/code\/core",path+ligne)):
+						continue
 
-					#load dans les templates
-					search_for_load =self.searchForLoad(path+ligne, dossierLog)
-					if(len(search_for_load) is not 0):
-						results['search_for_load'].append(search_for_load)
+					#si dans le code
+					if(re.search(r"app\/code",path+ligne)):
 
-					#getblock dans les templates
-					search_for_getblock =self.searchForGetblock(path+ligne, dossierLog)
-					if(len(search_for_getblock) is not 0):
-						results['search_for_getblock'].append(search_for_getblock)
+						#ici on est dans chaque fichier PHP
+						if(ligne.endswith('.php')):
+						
+							#new dans le  code
+							search_for_new =self.searchForNew(path+ligne, dossierLog,1)
+							if(len(search_for_new) is not 0):
+								results['code_search_for_new'].append(search_for_new)
 
-					#createblock dans les templates
-					search_for_createblock =self.searchForCreateblock(path+ligne, dossierLog)
-					if(len(search_for_createblock) is not 0):
-						results['search_for_createblock'].append(search_for_createblock)
+							#globalPHP dans le  code
+							search_for_globalphp =self.searchForPhpGlobals(path+ligne, dossierLog)
+							if(len(search_for_globalphp) is not 0):
+								results['code_global_php'].append(search_for_globalphp)
+
+							#fonctions mysql_ dans le code
+							search_for_mysql =self.searchForMysql(path+ligne, dossierLog)
+							if(len(search_for_mysql) is not 0):
+								results['code_mysql'].append(search_for_mysql)
+
+							#A FAIRE LOAD COUTEUX
+
+					#si dans design
+					if(re.search(r"app\/design",path+ligne)):	
+			
+						#ici on est dans chaque fichier du dossier
+						if(ligne.endswith('.phtml')):
+
+							#load dans les templates
+							search_for_load =self.searchForLoad(path+ligne, dossierLog)
+							if(len(search_for_load) is not 0):
+								results['template_search_for_load'].append(search_for_load)
+
+							#getblock dans les templates
+							search_for_getblock =self.searchForGetblock(path+ligne, dossierLog)
+							if(len(search_for_getblock) is not 0):
+								results['template_search_for_getblock'].append(search_for_getblock)
+
+							#createblock dans les templates
+							search_for_createblock =self.searchForCreateblock(path+ligne, dossierLog)
+							if(len(search_for_createblock) is not 0):
+								results['template_search_for_createblock'].append(search_for_createblock)
 					
-					#new dans les templates
-					search_for_new =self.searchForNew(path+ligne, dossierLog)
-					if(len(search_for_new) is not 0):
-						results['search_for_new'].append(search_for_new)
+							#new dans les templates
+							search_for_new =self.searchForNew(path+ligne, dossierLog, 0)
+							if(len(search_for_new) is not 0):
+								results['template_search_for_new'].append(search_for_new)
+
+							#globalPHP dans les templates
+							search_for_globalphp =self.searchForPhpGlobals(path+ligne, dossierLog)
+							if(len(search_for_globalphp) is not 0):
+								results['template_global_php'].append(search_for_globalphp)
+
+							#fonctions mysql_ dans le code
+							search_for_mysql =self.searchForMysql(path+ligne, dossierLog)
+							if(len(search_for_mysql) is not 0):
+								results['template_mysql'].append(search_for_mysql)
+
+							
 	
-
-				#elif(ligne.endswith('.xml')):	
-
 		return results
 
 	#
@@ -93,7 +136,7 @@ class Audit:
 		retoursAll=[]
 		for ligne in fichier:
 			nbrLigne+=1	
-			result = re.search("->load\(",ligne)					
+			result = re.search(r"->load\(",ligne)					
 			if result is not None:
 				retours = {}
 				retours['path'] = path
@@ -114,8 +157,50 @@ class Audit:
 		retoursAll=[]
 		for ligne in fichier:
 			nbrLigne+=1	
-			result = re.search("->getBlock\(",ligne)					
+			result = re.search(r"->getBlock\(",ligne)					
 			if result is not None:
+				retours = {}
+				retours['path'] = path
+				retours['ligne'] = str(nbrLigne)  
+				retours['contents']=ligne.strip(" \t\n\r")	
+				retoursAll.append(retours)
+				nbrLoads+=1
+		return retoursAll
+
+	#
+	# Fonction qui repere les getBlock dans les templates
+	#
+	def searchForPhpGlobals(self, path, dossierLog):
+		fichier = open(path, 'r')
+		nbrLigne=0	
+		nbrLoads=0		
+		retoursAll=[]
+		for ligne in fichier:
+			nbrLigne+=1	
+			post = re.search(r"\$_POST",ligne)
+			get = re.search(r"\$_GET",ligne)
+			glo = re.search(r"\$_GLOBAL",ligne)	
+			if ( (post is not None) or (get is not None) or (glo is not None) ):
+				retours = {}
+				retours['path'] = path
+				retours['ligne'] = str(nbrLigne)  
+				retours['contents']=ligne.strip(" \t\n\r")	
+				retoursAll.append(retours)
+				nbrLoads+=1
+		return retoursAll
+
+	#
+	# Fonction qui repere les fonctions mysql non magento dans le code
+	#
+	def searchForMysql(self, path, dossierLog):
+		fichier = open(path, 'r')
+		nbrLigne=0	
+		nbrLoads=0		
+		retoursAll=[]
+		for ligne in fichier:
+			nbrLigne+=1	
+			mysql = re.search(r"mysql_",ligne)	
+			if ( (mysql is not None)):
 				retours = {}
 				retours['path'] = path
 				retours['ligne'] = str(nbrLigne)  
@@ -134,7 +219,7 @@ class Audit:
 		retoursAll=[]
 		for ligne in fichier:
 			nbrLigne+=1	
-			result = re.search("->createBlock\(",ligne)
+			result = re.search(r"->createBlock\(",ligne)
 			resultWidgetName = re.search("customer/widget_",ligne)					
 			if ( result is not None ) and ( resultWidgetName is None ):
 				retours = {}
@@ -145,10 +230,13 @@ class Audit:
 				nbrLoads+=1
 		return retoursAll
 
+
 	#
 	# Fonction qui repere les new dans les templates
+	# si code=1 on est dans le code
+	# si code=0 on est dans les templates
 	#
-	def searchForNew(self, path, dossierLog):
+	def searchForNew(self, path, dossierLog, code):
 		fichier = open(path, 'r')
 		nbrLigne=0	
 		nbrLoads=0
@@ -161,6 +249,18 @@ class Audit:
 			inscript_three = re.search("<script>",ligne)
 			outcript = re.search("</script",ligne)		
 			intraduct = re.search("__\(",ligne)
+
+			if(code is 1):
+				varien = re.search("new Varien",ligne)
+				stdclass = re.search("new StdClass",ligne)
+				zend = re.search("new Zend",ligne)	
+				exception = re.search("Exception",ligne)
+				soap = re.search("new SoapClient",ligne)	
+				simplexml = re.search("new SimpleXMLElement",ligne)
+				arrayiterator = re.search("new ArrayIterator",ligne)
+				stdclassbis = re.search("new stdClass",ligne)	
+				date = re.search("DateTime",ligne)
+			
 
 			incomment = re.search("\/\*",ligne)
 			outcomment = re.search("\*\/",ligne)
@@ -178,12 +278,13 @@ class Audit:
 			nbrLigne+=1	
 			result = re.search("new ",ligne)				
 			if ( result is not None ) and ( isInScript is not  1) and (intraduct is None)  and (isInComment is not 1):
-				retours = {}
-				retours['path'] = path
-				retours['ligne'] = str(nbrLigne)  
-				retours['contents']=ligne.strip(" \t\n\r")	
-				retoursAll.append(retours)
-				nbrLoads+=1
+				if( (code is not 1) or ((varien is None) and (stdclass is None) and (zend is None) and (exception is None)  and (soap is None) and (exception is None)  and (simplexml is None) and (arrayiterator is None) and (stdclassbis is None) and (date is None))):
+					retours = {}
+					retours['path'] = path
+					retours['ligne'] = str(nbrLigne)  
+					retours['contents']=ligne.strip(" \t\n\r")	
+					retoursAll.append(retours)
+					nbrLoads+=1
 		return retoursAll
 
 
